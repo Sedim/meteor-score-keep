1749393a7f5f0520d42de28a1465a937a8863e5e
Sedim Al-Araji <sedim@launchit.com>
2017-08-31 20:33:57 -0400

    test again

--- server/main.js
+++ server/main.js
@@ -1,7 +1,9 @@
 // import './../imports/utils'
+//
 import myDefaultVar, {greetUser} from './../imports/utils'
 
 console.log('From /server/main.js')
 console.log(greetUser())
 console.log(myDefaultVar)
+
 
