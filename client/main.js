9d99a5d2340baa2011946695e0f313fea5d4dd8b
Sedim Al-Araji <sedim@launchit.com>
2017-09-03 22:54:39 -0400

    Modify mongodb players woth delete/add/subtract points, add React component through class

--- client/main.js
+++ client/main.js
@@ -1,49 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { meteor } from 'meteor/meteor';
+import {meteor} from 'meteor/meteor';
 
 import {Players} from '../imports/api/players';
+import TitleBar from  '../imports/ui/TitleBar';
+import AddPlayer from  '../imports/ui/AddPlayer';
 
-Tracker.autorun(()=> console.log('Players list', Players.find().fetch()));
-// Tracker.autorun(function () {
-//     console.log('Players list', Players.find().fetch());
-//   });
-  
-const players = [{
-    _id: '1',
-    name: 'Lauren',
-    score: 99
-}, {
-    _id: '2',
-    name: 'Cory',
-    score: -1
-}, {
-    _id: '3',
-    name: 'Andrew',
-    score: -12
-}];
+const renderPlayers = (params) =>
+  params.map((user) => (
+    <p key={user._id}>
 
-const renderPlayers = function (params) {
-    let newUsers = params.map((user) => <p key= {user._id}>{user.name} has {user.score -1} points</p>)
-    // let numbers = [3, 4, 5];
-    // console.log (JSON.stringify(params))
-    // let newNumbers = numbers.map((number) => <p key= {number}>{number}</p>)
-    // console.log(JSON.stringify(newUsers));
-    // console.log(newUsers);
-    // return [<p key="1">1</p>, <p key="2">2</p>, <p key="3">5</p>,]
-    return newUsers;
+      {user.name} has {user.score} points
+
+    <button onClick={() => {
+        //{user.score --}
+        Players.update({_id: user._id}, {$inc: {score: -1}}) // $inc directly example
+      }}>-1</button>
+
+      <button onClick={() => {
+        {user.score++}
+        Players.update(user._id, {$set: {score: user.score}}) // $set directly exampl
+      }}>+1</button>
+
+      <button onClick={() => {
+        //alert("I am an alert box!")
+        Players.remove(user._id) // use the object _id directly syntax without _id: 
+      }}>X</button>
+
+    </p>
+  )
+  );
+
+const handleSubmit = (e) => {
+  let playerName = e.target.playerName.value;
+
+  e.preventDefault();
+
+  if (playerName) {
+    e.target.playerName.value = '';// This clears the field you 
+    Players.insert({name: playerName, score: 0});
+    console.log(Players.find().fetch());
+  };
 };
 
-Meteor.startup(function () {
+
+Meteor.startup(() => {
+  Tracker.autorun(() => {
+    const players = Players.find().fetch();
     let title = 'Score Keep';
     let name = 'Sedim';
     let jsx = (
-    <div>
-        <h1>{title}</h1>
-        <p>This is from {name}</p>
-        <p>This is my second p</p> 
+      <div>
+        <TitleBar/>
         {renderPlayers(players)}
-    </div>
+        <AddPlayer/>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="playerName" placeholder="Player name" />
+          <button>Add Player</button>
+        </form>
+      </div>
+
     );
     ReactDOM.render(jsx, document.getElementById('app'));
+  });
 });
